d79b7562b35302db551ed507d49a3826a3b07369
Vincent STRAGIER <vincent.stragier97@gmail.com>
2022-03-07 18:49:27 +0100

    Fix get likes counter error.

--- facebook_scraping.py
+++ facebook_scraping.py
@@ -5,57 +5,31 @@
     Requirement:
     python3 -m pip install pillow facebook_scraper, demoji, emoji
 """
+import re
 import requests
 
 # https://pypi.org/project/demoji/
 import demoji
-from contextlib import redirect_stdout
-import io
+# from contextlib import redirect_stdout
+# import io
 
 import emoji
 
 # https://pypi.org/project/facebook-scraper/
-from facebook_scraper import get_posts
+from facebook_scraper import get_posts  # , set_user_agent
 
 
 # Get the number of likes on the Facebook page (using the name of the page)
 def getLikeCount(page_name):
-    """
-    URL = "https://fr-fr.facebook.com/pg/" + PAGE_NAME
-    + "/community/?ref=page_internal"
+    URL = f"https://fr-fr.facebook.com/pg/{page_name}"
 
-    LOWER_DELIMITER_0 = "Total des mentions J’aime</div>"
-    LOWER_DELIMITER_1 = "</div>"
-    UPPER_DELIMITER_0 = "\">"
-
-    r = requests.get(URL)
-    page_text = r.text
-    page_text = page_text[:page_text.find(LOWER_DELIMITER_0)]
-    page_text = page_text[:page_text.rfind(LOWER_DELIMITER_1)]
-    page_text = page_text[page_text.rfind(UPPER_DELIMITER_0)
-    + len(UPPER_DELIMITER_0):]
-    """
-    # <div>636&nbsp;124 personnes aiment ça</div>
-    URL = "https://fr-fr.facebook.com/pg/" + page_name
-    LOWER_DELIMITER_0 = " personnes aiment ça</div>"
-    UPPER_DELIMITER_0 = "<div>"
-    SPACE = b'\xc2\xa0'.decode('UTF-8')  # Remove space
-
-    r = requests.get(URL)
-    page_text = r.text
-    page_text = page_text[:page_text.rfind(LOWER_DELIMITER_0)]
-    page_text = page_text[page_text.rfind(UPPER_DELIMITER_0) + len(
-        UPPER_DELIMITER_0):].replace(SPACE, '').replace(' ', '')
-    return page_text if page_text == str(int(page_text))\
-        else "Error: check page name"
-
+    response = requests.get(URL)
+    description = re.findall("<meta name=\"description\".+/>", response.text)
+    count = re.findall("([0-9]+)[\s]+J&#x2019;aime", description[0])[0]
+    return count if count else -1
 
 # Remove the emoji from the 'data' string
 def remove_emoji(data):
-    f = io.StringIO()
-    with redirect_stdout(f):     # Hide demoji stdout (stored in f.getvalue())
-        demoji.download_codes()  # Update emoji list
-
     return demoji.replace(data)
 
 
@@ -66,6 +40,7 @@ def add_emoji(emoji_name):
 
 # Get the first post on the 'page_name' Facebook Page containing the 'key'
 def getPost(page_name, key, enable_emoji=True):
+    # set_user_agent("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
     for post in get_posts(page_name):
         if key in post['text'][:]:  # Looking for the clock emoji
             if enable_emoji:
